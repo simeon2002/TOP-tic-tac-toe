17a617037de5113f291144301e10a9a6f25d55be
Simeon <61100437+simeon2002@users.noreply.github.com>
2026-08-10 23:47:13 +0300

    Set up game, player and gameBoard objects and functionality to initialize a game

--- script.js
+++ script.js
@@ -1,36 +1,121 @@
 "use strict";
 
-// Game module pattern function
-const Game = (function () {
-  return "test";
-})();
-
-console.log(Game);
-
 // Player factory function
 const createPlayer = function (name, marker) {
   let score = 0;
 
-  function makeMove() {}
+  function getScore() {
+    return score;
+  }
+
+  function getName() {
+    return name;
+  }
+
+  function getMarker() {
+    return marker;
+  }
+
+  function printPlayerInfo() {
+    console.log(`${name}: ${marker}`);
+  }
+
+  return { getScore, getName, getMarker, printPlayerInfo };
 };
 
 const createGameBoard = function (rows, columns) {
-  let board = new Array(rows);
-  init();
+  let board;
 
   // Initialize gameboard
   function init() {
+    generateArray(rows, columns);
+  }
+
+  /**
+   * Print board
+   */
+  function printBoard() {
+    const boardStr = board.reduce((str, row) => {
+      str += printCols(row) + "\n";
+      return str;
+    }, "");
+    console.log(boardStr);
+  }
+
+  /**
+   * Print columns with additonal padding to format the column nicely
+   * @param {number} row row array to print columns from
+   * @param {number} padCount max length of the string, default = 6
+   * @param {string} padValue Value to pad with, default = " "
+   * @param {string} separator Value to separate the columns with
+   * @returns
+   */
+  function printCols(row, padCount = 6, padValue = " ", separator = "|") {
+    // const subStr = " ".padEnd(padCount, padValue);
+    let str = row
+      .reduce((str, el) => str + el.padStart(padCount / 2, padValue).padEnd(padCount, padValue) + separator, "")
+      .padEnd(padCount, padValue);
+    return str;
+  }
+
+  /**
+   * Generates a new array with row rows and cols columns
+   * @param {number} row number of rows for the array
+   * @param {number} cols Number of columns for the array
+   * @param {any} value Value to fill within
+   */
+  function generateArray(row = 3, cols = 3, value = "") {
+    board = new Array(rows);
+
     for (let i = 0; i < board.length; i++) {
-      board[i] = new Array(columns);
+      board[i] = new Array(cols).fill(value);
     }
   }
 
-  return { board };
+  return { init, printBoard };
 };
 
-// create a game board
-const board = createGameBoard(3, 4);
+// Game module pattern function
+const game = (function () {
+  let board, players, currentPlayer;
 
-// create two players
-const player1 = createPlayer("Simeon", "X");
-const player2 = createPlayer("Darina", "O");
+  // Initialize a game
+  function startGame(...newPlayers) {
+    // init state
+    board = createGameBoard(3, 3);
+    board.init();
+    players = newPlayers;
+    currentPlayer = 0;
+
+    displayInitialState();
+  }
+
+  function displayInitialState() {
+    console.log("Starting a game...");
+    players.forEach(player => player.printPlayerInfo());
+    board.printBoard();
+  }
+
+  /**
+   * Players' plays a turn on the board
+   *
+   */
+
+  // gettter and setter methods
+  function getBoard() {
+    return board;
+  }
+
+  function getPlayersScore() {
+    return players.map(player => ({ [player.getName()]: player.getScore() }));
+  }
+
+  function getPlayers() {
+    return players;
+  }
+
+  return { startGame, getBoard, getPlayersScore, getPlayers };
+})();
+
+// start a game
+game.startGame(createPlayer("Simeon", "O"), createPlayer("Darina", "X"));
